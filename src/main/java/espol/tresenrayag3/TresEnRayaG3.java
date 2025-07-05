1c4bc73eb49b3e26c1bc00b1200bdfb12721410e
Maycol Mont <maycol.montalvan1405@gmail.com>
2025-07-05 12:52:46 -0500

    first commit

---
+++ src/main/java/espol/tresenrayag3/TresEnRayaG3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espol.tresenrayag3;
+
+/**
+ *
+ * @author elmay
+ */
+public class TresEnRayaG3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
